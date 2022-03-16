e7287a24eccb5e1c640f2fe6ef40e4bf5d2286b6
Juan Carlos <juancferreira54@gmail.com>
2022-03-16 18:34:24 -0300

    revisada a tela de exclusao de curso

--- includes/confirmar-exclusao.php
+++ includes/confirmar-exclusao.php
@@ -1,20 +1,18 @@
 <main>
 
-
-    <h2 class='mt-3'>Excluir curso</h2>
+<div class='justify-content-center d-flex flex-align align-items-center' align='center'>
+    
 
     <form method='post'>
-        <div class='form-group'>
-           <p>Você realmente deseja excluir o curso <strong><?=$obCurso->titulo ?></strong>?</p>
-        </div>
-
         
-        <div class='form-group'>
+            <h1 class='mt-3'>Excluir curso</h1>
+            <p>Você realmente deseja excluir o curso <strong><?=$obCurso->titulo ?></strong>?</p>
+
             <a href='index.php'>
                 <button type='button' class='btn btn-primary mt-3'>Cancelar</button>
             </a>
             <button type='submit' name='excluir' class='btn btn-danger mt-3'>Excluir</button>
-        </div>
-    </form>
 
+    </form>
+</div>
 </main>
